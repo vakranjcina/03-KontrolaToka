8a536e8d2954b575d52ccb316a7c76185819552c
vakranjcina <valentina.kranjcina@hotmail.com>
2015-11-05 20:40:14 +0100

    UvjetnoPridruzivanje todo1 solved

--- UvjetnoPridruživanje/UvjetnoPridruživanje.cs
+++ UvjetnoPridruživanje/UvjetnoPridruživanje.cs
@@ -13,15 +13,18 @@ namespace Vsite.CSharp
             {
                 int broj = int.Parse(unos);
 
-                // TODO: Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-                string odgovor = "paran";
+                // Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
+                string odgovor = broj % 2 == 0 ? "paran" : "neparan";
+
 
                 Console.WriteLine("Broj {0} je {1}", broj, odgovor);
 
                 // TODO: Napisati isto pridruživanje ali korištenjem uvjetnog grananja if
-                string odgovor2 = "paran";
+                //string odgovor2 = "paran";
+                //if (odgovor2 = broj % 2 == 0 ? "paran" : "neparan")
+                
 
-                Console.WriteLine("Broj {0} je {1}", broj, odgovor2);
+                //Console.WriteLine("Broj {0} je {1}", broj, odgovor2);
 
             }
             catch (Exception)
