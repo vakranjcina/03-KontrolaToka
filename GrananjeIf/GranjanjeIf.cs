b31889af505eab9c91af6a86865b5701fa1f21a9
vakranjcina <valentina.kranjcina@hotmail.com>
2015-11-05 20:18:32 +0100

    Grananje If odraden TODO

--- GrananjeIf/GranjanjeIf.cs
+++ GrananjeIf/GranjanjeIf.cs
@@ -13,12 +13,23 @@ namespace Vsite.CSharp
             {
                 int broj = int.Parse(unos);
 
-                // TODO: Napisati grananja if koja će za uneseni broj:
+                // Napisati grananja if koja će za uneseni broj:
                 // 1. provjeriti je li broj paran. Ako je broj paran, ispisat će poruku "broj N je djeljiv s 2", a inače će
                 // 2. provjeriti je li broj djeljiv s 3. Ako je broj djeljiv, ispisat će poruku "broj N je djeljiv s 3", a inače će
                 // 3. provjeriti je li broj djeljiv s 5. Ako je broj djeljiv, ispisat će poruku "broj N je djeljiv s 5", a inače će
                 //    ispisati da nije djeljiv niti s jednim brojem
-
+                if (broj % 2 == 0){
+                    Console.WriteLine("Broj {0} je djeljiv s 2", broj);
+                }
+                else if (broj % 3 == 0) {
+                    Console.WriteLine("Broj {0} je djeljiv s 3", broj);
+                }
+                else if (broj % 5 == 0){
+                    Console.WriteLine("Broj {0} je djeljiv s 5", broj);
+                }
+                else {
+                    Console.WriteLine("Broj {0} nije djeljiv ni s jednim brojem", broj);
+                }
 
             }
             catch (Exception)
